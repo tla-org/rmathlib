69883e67e9109bbd773cb57820c6995bc05f0999
Rik Huijzer <github@huijzer.xyz>
2024-01-23 12:05:59 +0100

    Add `sinpi` and `tanpi` (#2)

--- test-math/src/lib.rs
+++ test-math/src/lib.rs
@@ -3,11 +3,18 @@ mod test_math {
 
     extern "C" {
         fn cospi(x: f64) -> f64;
+        fn sinpi(x: f64) -> f64;
+        fn tanpi(x: f64) -> f64;
     }
 
     #[test]
     fn test_cospi() {
         assert_eq!(math::cospi(0.0), unsafe { cospi(0.0) });
         assert_eq!(math::cospi(0.234), unsafe { cospi(0.234) });
+        assert_eq!(math::sinpi(0.0), unsafe { sinpi(0.0) });
+        assert_eq!(math::sinpi(0.234), unsafe { sinpi(0.234) });
+        assert_eq!(math::tanpi(0.0), unsafe { tanpi(0.0) });
+        assert_eq!(math::tanpi(0.25), unsafe { tanpi(0.25) });
+        assert_eq!(math::tanpi(0.234), unsafe { tanpi(0.234) });
     }
 }
