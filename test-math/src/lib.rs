254e5c8c4afcb9047684a6c81cb1ce7b7d411514
Rik Huijzer <github@huijzer.xyz>
2024-01-23 15:49:53 +0100

    Apply fmt

--- test-math/src/lib.rs
+++ test-math/src/lib.rs
@@ -26,7 +26,11 @@ mod test_math {
     fn test_pnorm() {
         assert_eq!(pnorm(0.0, 0.0, 1.0, true, false), 0.5);
         assert_eq!(pnorm(0.0, 0.0, 1.0, false, false), 0.5);
-        assert_eq!(pnorm(0.0, 0.0, 1.0, false, false), unsafe { c::pnorm5(0.0, 0.0, 1.0, false, false) });
-        assert_eq!(pnorm(0.65, 0.2, 0.34, false, false), unsafe { c::pnorm5(0.65, 0.2, 0.34, false, false) });
+        assert_eq!(pnorm(0.0, 0.0, 1.0, false, false), unsafe {
+            c::pnorm5(0.0, 0.0, 1.0, false, false)
+        });
+        assert_eq!(pnorm(0.65, 0.2, 0.34, false, false), unsafe {
+            c::pnorm5(0.65, 0.2, 0.34, false, false)
+        });
     }
 }
