c6d18336a539568e388fe3a55accef76b61e33b7
Jose Storopoli <jose@storopoli.io>
2024-01-25 05:43:59 -0300

    chore: add tests

--- test-math/src/lib.rs
+++ test-math/src/lib.rs
@@ -10,6 +10,13 @@ mod test_math {
             pub fn tanpi(x: f64) -> f64;
             pub fn pnorm5(x: f64, mu: f64, sigma: f64, lower_tail: i32, log_p: i32) -> f64;
             pub fn qnorm5(p: f64, mu: f64, sigma: f64, lower_tail: i32, log_p: i32) -> f64;
+            // pub fn pgamma(x: f64, alph: f64, scale: f64, lower_tail: bool, log_p: bool) -> f64; // TODO: tests
+            pub fn lgammafn(x: f64) -> f64;
+            pub fn lgammafn_sign(x: f64, sgn: Option<&mut i32>) -> f64;
+            pub fn gammafn(x: f64) -> f64;
+            pub fn lgammacor(x: f64) -> f64;
+            pub fn chebyshev_init(dos: *mut f64, eta: f64) -> usize;
+            pub fn chebyshev_eval(x: f64, a: *mut f64, n: usize) -> f64;
         }
     }
 
@@ -61,5 +68,88 @@ mod test_math {
             unsafe { c::qnorm5(-2.3, 0.5, 1.0, 0, 1) },
             epsilon = 1e-15
         ));
+    }
+
+    #[test]
+    fn test_lgammafn() {
+        assert_eq!(lgammafn(0.0), unsafe { c::lgammafn(0.0) });
+        assert_eq!(lgammafn(-1.0), unsafe { c::lgammafn(-1.0) });
+        assert_eq!(lgammafn(1.0), unsafe { c::lgammafn(1.0) });
+    }
+
+    #[test]
+    fn test_lgammafn_sign() {
+        assert_eq!(lgammafn_sign(0.0, Some(&mut -1)), unsafe {
+            c::lgammafn_sign(0.0, Some(&mut -1))
+        });
+        assert_eq!(lgammafn_sign(-1.0, Some(&mut -1)), unsafe {
+            c::lgammafn_sign(-1.0, Some(&mut -1))
+        });
+        assert_eq!(lgammafn_sign(1.0, Some(&mut -1)), unsafe {
+            c::lgammafn_sign(1.0, Some(&mut -1))
+        });
+        assert_eq!(lgammafn_sign(0.0, Some(&mut 1)), unsafe {
+            c::lgammafn_sign(0.0, Some(&mut 1))
+        });
+        assert_eq!(lgammafn_sign(-1.0, Some(&mut 1)), unsafe {
+            c::lgammafn_sign(-1.0, Some(&mut 1))
+        });
+        assert_eq!(lgammafn_sign(1.0, Some(&mut 1)), unsafe {
+            c::lgammafn_sign(1.0, Some(&mut 1))
+        });
+    }
+
+    #[test]
+    fn test_gammafn() {
+        assert!(gammafn(-1.0).is_nan());
+        assert!(gammafn(0.0).is_nan());
+        assert_eq!(gammafn(0.1), unsafe { c::gammafn(0.1) });
+        assert_eq!(gammafn(1.0), unsafe { c::gammafn(1.0) });
+    }
+
+    #[test]
+    fn test_lgammacor() {
+        assert!(lgammacor(-1.0).is_nan());
+        assert!(lgammacor(0.0).is_nan());
+        assert_eq!(lgammacor(10.0), unsafe { c::lgammacor(10.0) });
+        assert_eq!(lgammacor(20.0), unsafe { c::lgammacor(20.0) });
+        assert_eq!(lgammacor(3.745194030963158e306 + 1.0), unsafe {
+            c::lgammacor(3.745194030963158e306 + 1.0)
+        });
+        assert_eq!(lgammacor(94906265.62425156 + 1.0), unsafe {
+            c::lgammacor(94906265.62425156 + 1.0)
+        });
+    }
+
+    #[test]
+    fn test_chebyshev_init() {
+        assert_eq!(chebyshev_init(&[1.0, 2.0, 3.0], 0.5).unwrap(), unsafe {
+            c::chebyshev_init([1.0, 2.0, 3.0].as_mut_ptr(), 0.5)
+        });
+        assert_eq!(chebyshev_init(&[], 0.5).unwrap(), unsafe {
+            c::chebyshev_init([].as_mut_ptr(), 0.5)
+        });
+        assert_eq!(chebyshev_init(&[1.0, 2.0, 3.0], -0.5).unwrap(), unsafe {
+            c::chebyshev_init([1.0, 2.0, 3.0].as_mut_ptr(), -0.5)
+        });
+    }
+
+    #[test]
+    fn test_chebyshev_eval() {
+        assert_eq!(chebyshev_eval(0.6, &[1.0, 2.0, 3.0], 2), unsafe {
+            c::chebyshev_eval(0.6, [1.0, 2.0, 3.0].as_mut_ptr(), 2)
+        });
+        assert_eq!(chebyshev_eval(0.6, &[1.0, 2.0, 3.0], 0), unsafe {
+            c::chebyshev_eval(0.6, [1.0, 2.0, 3.0].as_mut_ptr(), 0)
+        });
+        assert_eq!(chebyshev_eval(0.6, &[1.0, 2.0, 3.0], 5), unsafe {
+            c::chebyshev_eval(0.6, [1.0, 2.0, 3.0].as_mut_ptr(), 5)
+        });
+        assert_eq!(chebyshev_eval(-0.6, &[1.0, 2.0, 3.0], 2), unsafe {
+            c::chebyshev_eval(-0.6, [1.0, 2.0, 3.0].as_mut_ptr(), 2)
+        });
+        assert_eq!(chebyshev_eval(0.6, &[], 2), unsafe {
+            c::chebyshev_eval(0.6, [].as_mut_ptr(), 2)
+        });
     }
 }
