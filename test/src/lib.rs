d4405c5a7006956a7c92b18dd1ca90d3b1886fed
Rik Huijzer <github@huijzer.xyz>
2024-01-26 21:22:47 +0100

    fmt

--- test/src/lib.rs
+++ test/src/lib.rs
@@ -239,9 +239,7 @@ mod test_math {
         let mut c_yh: f64 = 0.0;
         let mut c_yl: f64 = 0.0;
         ebd0(1.1, 2.2, &mut yh, &mut yl);
-        unsafe {
-            c::Rf_ebd0(1.1, 2.2, &mut c_yh, &mut c_yl)
-        };
+        unsafe { c::Rf_ebd0(1.1, 2.2, &mut c_yh, &mut c_yl) };
         // assert_eq!(yh, c_yh);
     }
 }
