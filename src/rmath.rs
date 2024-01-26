fae2417d455376a9994f08a48018a64973bc694e
Rik Huijzer <github@huijzer.xyz>
2024-01-26 10:12:16 +0100

    fmt

--- src/rmath.rs
+++ src/rmath.rs
@@ -9,7 +9,6 @@ pub const M_SQRT_32: f64 = 5.656_854_249_492_381;
 pub const M_1_SQRT_2PI: f64 = 0.398_942_280_401_432_7; // 1/sqrt(2pi)
 pub const ML_LN2: f64 = LN_2;
 
-
 pub fn pnorm(x: f64, mu: f64, sigma: f64, lower_tail: bool, log_p: bool) -> f64 {
     pnorm5(x, mu, sigma, lower_tail, log_p)
 }
