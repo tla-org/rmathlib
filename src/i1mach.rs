bae5f0f1311a9868069e0ddc3132e0c09c698ef2
Jose Storopoli <jose@storopoli.io>
2024-01-26 07:56:23 -0300

    fix: format

--- src/i1mach.rs
+++ src/i1mach.rs
@@ -15,14 +15,14 @@ pub fn rf_i1mach(i: i32) -> i32 {
         6 => size_of::<c_int>() as i32 / (CHAR_BIT / 8), // sizeof(int)/sizeof(char)
         7 => 2,
         8 => CHAR_BIT * size_of::<c_int>() as i32 - 1, // CHAR_BIT * sizeof(int) - 1
-        9 => i32::MAX,                          // INT_MAX
+        9 => i32::MAX,                                 // INT_MAX
         10 => f32::RADIX as i32,                       // FLT_RADIX
         11 => f32::MANTISSA_DIGITS as i32,             // FLT_MANT_DIG
-        12 => f32::MIN_EXP,                     // FLT_MIN_EXP
-        13 => f32::MAX_EXP,                     // FLT_MAX_EXP
+        12 => f32::MIN_EXP,                            // FLT_MIN_EXP
+        13 => f32::MAX_EXP,                            // FLT_MAX_EXP
         14 => f64::MANTISSA_DIGITS as i32,             // DBL_MANT_DIG
-        15 => f64::MIN_EXP,                     // DBL_MIN_EXP
-        16 => f64::MAX_EXP,                     // DBL_MAX_EXP
+        15 => f64::MIN_EXP,                            // DBL_MIN_EXP
+        16 => f64::MAX_EXP,                            // DBL_MAX_EXP
         _ => 0,
     }
 }
