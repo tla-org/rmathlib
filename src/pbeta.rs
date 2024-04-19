c43e95c04e8f14d2d055112609ea50276d13a407
Rik Huijzer <github@huijzer.xyz>
2024-04-19 17:00:22 +0200

    fmt

--- src/pbeta.rs
+++ src/pbeta.rs
@@ -41,7 +41,10 @@ fn pbeta_raw(x: f64, a: f64, b: f64, lower_tail: bool, log_p: bool) -> f64 {
     bratio(a, b, x, x1, &mut w, &mut w1, &mut ierr, log_p);
 
     if ierr != 0 && ierr != 11 && ierr != 14 {
-        println!("pbeta_raw({}, a={}, b={}, lower_tail={}, log_p={}) -> bratio() gave error code{}", x, a, b, lower_tail, log_p, ierr);
+        println!(
+            "pbeta_raw({}, a={}, b={}, lower_tail={}, log_p={}) -> bratio() gave error code{}",
+            x, a, b, lower_tail, log_p, ierr
+        );
     }
     if lower_tail {
         w
