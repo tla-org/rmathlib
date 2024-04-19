0089bff6708d1ad37b9e4e72cf9e67e3a2da3f75
Rik Huijzer <github@huijzer.xyz>
2024-04-19 16:49:22 +0200

    Re-enable another test

--- test/src/lib.rs
+++ test/src/lib.rs
@@ -301,10 +301,6 @@ mod test_math {
             epsilon = epsilon
         );
 
-        println!("\nEntering problematic test.\n");
-        // I have no idea what the problem is here.
-        //
-        // helper(0.1, 0.8, 2.0, false, true);
         assert_abs_diff_eq!(
             pbeta(0.1, 0.8, 2.0, false, true),
             // R> sprintf("%.13f", pbeta(0.1, 0.8, 2.0, lower.tail=FALSE, log.p=TRUE))
@@ -313,13 +309,13 @@ mod test_math {
         );
 
         // Based on a test in `d-p-q-r-tst-2.R` from the R source code.
-        // for x in [0.01, 0.10, 0.25, 0.40, 0.55, 0.71, 0.98] {
-        //     assert_abs_diff_eq!(
-        //         pbeta(x, 0.8, 2.0, false, true),
-        //         pbeta(1.0 - x, 2.0, 0.8, true, true),
-        //         epsilon = epsilon,
-        //     )
-        // }
+        for x in [0.01, 0.10, 0.25, 0.40, 0.55, 0.71, 0.98] {
+            assert_abs_diff_eq!(
+                pbeta(x, 0.8, 2.0, false, true),
+                pbeta(1.0 - x, 2.0, 0.8, true, true),
+                epsilon = epsilon,
+            )
+        }
 
         // Tests via the helper function.
         helper(0.1, 0.5, 0.5, false, false);
